dede3f10e0e6adcd68baf07a67aac8b9091cc433
mishamolnar <michaellmuller423@gmail.com>
2021-07-23 18:02:19 +0300

    testing ptoject

--- src/main/java/hello_app.java
+++ src/main/java/hello_app.java
@@ -2,7 +2,8 @@ public class hello_app {
     public static void print() {
 
 
-        System.out.println("Hello, mates")
+        System.out.println("Hello, mates");
+        System.out.println("Hello, mates");
     }
 
 }
